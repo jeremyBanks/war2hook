743279a83eaa759d873ca7671c458202d1f43402
Jeremy Banks <_@jeremy.ca>
2024-11-20 18:30:16 -0500

    failign to install hook

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use {
+    iced_x86::{self, code_asm::CodeAssembler},
     std::{
         ffi::{c_char, CString},
         fs::OpenOptions,
@@ -31,6 +32,18 @@ extern "system" fn DllMain(dll_module: HINSTANCE, call_reason: u32, _: *mut ()) 
     true
 }
 
+#[no_mangle]
+extern fn apply_cheats_hook(newCheats: u32, _2: i32) {
+    let mut hook_log = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open("C:\\Users\\_\\war2hook\\hook.txt")
+        .expect("Unable to open log file");
+
+    writeln!(hook_log, "you did it! it worked!").unwrap();
+}
+
 fn attach() {
     let mut log = OpenOptions::new()
         .write(true)
@@ -39,13 +52,44 @@ fn attach() {
         .open("C:\\Users\\_\\war2hook\\log.txt")
         .expect("Unable to open log file");
 
+    writeln!(log, "assembling hook!").unwrap();
+
+    let call_hook = {
+        use iced_x86::code_asm::*;
+
+        let mut a = CodeAssembler::new(32).unwrap();
+
+        a.call(ptr(apply_cheats_hook as *const (u32, i32) as u32))
+            .unwrap();
+
+        a.ret().unwrap();
+
+        a.assemble(0).unwrap()
+    };
+
+    // We're hooking the "day" cheat code, by overwriting the
+    // instructions at 0x4_160a4 with our assembled bytes. It returns,
+    // so we don't have to worry about corrupting the subsequent
+    // instructions if ours doesn't align.
+    let target: &mut [u8] =
+        unsafe { std::slice::from_raw_parts_mut(std::mem::transmute(0x4_160A4), 1000) };
+
+    writeln!(log, "installing hook!").unwrap();
+
+    target.copy_from_slice(&call_hook);
+
+    writeln!(log, "installed hook!").unwrap();
+
     std::thread::spawn(move || {
         let gold = unsafe { VolatilePtr::new(NonNull::new_unchecked(0x4_ABB18 as *mut u32)) };
         let lumber = unsafe { VolatilePtr::new(NonNull::new_unchecked(0x4_ACB6C as *mut u32)) };
         let oil = unsafe { VolatilePtr::new(NonNull::new_unchecked(0x4_ABBFC as *mut u32)) };
 
-        let displayMessage: extern fn(message: *const i8, _2: u8, _3: u32) =
-            unsafe { std::mem::transmute(0x0042ca40) };
+        let apply_cheats: extern fn(newCheats: u32, _2: i32) =
+            unsafe { std::mem::transmute(0x4_15EC0) };
+
+        let display_message: extern fn(message: *const i8, _2: u8, _3: u32) =
+            unsafe { std::mem::transmute(0x4_2CA40) };
 
         let mut last_line = String::new();
 
@@ -62,12 +106,12 @@ fn attach() {
                 last_line = line;
             }
 
-            if current_gold > 0 {
+            if current_gold > 0 && current_gold < 1337 {
                 gold.write(1337);
                 lumber.write(1337);
                 oil.write(1337);
 
-                displayMessage(CString::new("let's top you up!").unwrap().as_ptr(), 0, 0);
+                display_message(c"Let's give you more resources!".as_ptr(), 8, 100);
             }
 
             std::thread::sleep(Duration::from_secs(1));
--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,9 @@ use {
 
 fn main() {
     println!("Launching game");
-    let mut process =
-        std::process::Command::new("D:\\Program Files\\Warcraft II\\Warcraft II BNE.exe")
-            .spawn()
-            .expect("failed to launch warcraft 2");
+    std::process::Command::new("D:\\Program Files\\Warcraft II\\Warcraft II BNE.exe")
+        .spawn()
+        .expect("failed to launch warcraft 2");
 
     thread::sleep(time::Duration::from_secs(6));
 
@@ -19,16 +18,10 @@ fn main() {
     let target_process = OwnedProcess::find_first_by_name("Warcraft II BNE.exe")
         .expect("failed to find WarCraft II process");
 
-    let dll_path = {
-        if target_process.is_x64().unwrap() {
-            "target\\x86_64-pc-windows-msvc\\debug\\war2hook.dll"
-        } else {
-            "target\\i686-pc-windows-msvc\\debug\\war2hook.dll"
-        }
-    };
+    let dll_path = "target\\i686-pc-windows-msvc\\debug\\war2hook.dll";
 
     let syringe = Syringe::for_process(target_process);
 
     println!("Injecting DLL");
-    let injected_payload = syringe.inject(dll_path).expect("failed to inject DLL");
+    syringe.inject(dll_path).expect("failed to inject DLL");
 }
