67799759db68c70a6c4a8fb0c7cd06f2bc6e2c90
Jeremy Banks <_@jeremy.ca>
2024-11-23 15:00:25 -0500

    m

--- injection/lib.rs
+++ injection/lib.rs
@@ -2,7 +2,10 @@ use {
     crate::war2types::*,
     errors::try_or_die,
     eyre,
-    iced_x86::{self, code_asm::CodeAssembler},
+    iced_x86::{
+        self,
+        code_asm::{ax, CodeAssembler},
+    },
     std::{
         cell::Cell,
         ffi::CString,
@@ -13,9 +16,7 @@ use {
         },
         time::Instant,
     },
-    windows::Win32::System::Memory::{
-        VirtualProtect, PAGE_EXECUTE_READWRITE, PAGE_PROTECTION_FLAGS,
-    },
+    windows::Win32::System::Memory::{VirtualProtect, PAGE_PROTECTION_FLAGS, PAGE_READWRITE},
 };
 
 mod dllmain;
@@ -85,33 +86,61 @@ extern fn main_loop_hook() {
 
 unsafe fn patch_asm(
     address: u32,
-    asm: impl Fn(&mut CodeAssembler) -> Result<(), eyre::Error>,
+    asm: impl Fn(&mut CodeAssembler, &mut CodeAssembler) -> Result<(), eyre::Error>,
 ) -> Result<(), eyre::Error> {
     logln!("Assembling  patch for 0x{address:0X}.");
 
-    let mut assembler = CodeAssembler::new(32)?;
+    let mut old_assembler = CodeAssembler::new(32)?;
+    let mut new_assembler = CodeAssembler::new(32)?;
 
-    asm(&mut assembler)?;
+    asm(&mut old_assembler, &mut new_assembler)?;
 
-    let assembled = assembler.assemble(u64::from(address))?;
+    let assembled_old = old_assembler.assemble(u64::from(address))?;
+    let assembled_new = new_assembler.assemble(u64::from(address))?;
 
-    let assembled_len = assembled.len();
+    let assembled_old_len = assembled_old.len();
+    let assembled_new_len = assembled_new.len();
 
-    logln!("Patching {assembled_len:3} bytes at 0x{address:0X}.");
+    // TODO: we should allow the old to be longer than the new, and that's how
+    // we deal with mis-alignment. As long as we know everything we're
+    // replacing, it's acceptable.
+    if assembled_old_len < assembled_new_len {
+        logln!(
+            "WARN: Assembled original instructions are shorter ({assembled_old_len}B) as \
+             assembled new instructions ({assembled_new_len}B)."
+        )
+    }
+
+    logln!("Patching {assembled_new_len:3} bytes at 0x{address:0X}.");
 
     let target_memory: &mut [u8] =
-        std::slice::from_raw_parts_mut(transmute(address), assembled_len);
+        std::slice::from_raw_parts_mut(transmute(address), assembled_new_len);
 
+    if target_memory != &assembled_old[..assembled_new_len] {
+        logln!(
+            "WARN: Assembled original instructions did not match actual instructions in memory."
+        );
+        logln!("  actual memory: {}", hex::encode(&target_memory));
+        logln!("  assembled old: {}", hex::encode(&assembled_old));
+        logln!("  assembled new: {}", hex::encode(&assembled_new));
+    }
+
+    let mut original_flags = PAGE_PROTECTION_FLAGS(0);
     VirtualProtect(
         transmute(address),
-        assembled.len(),
-        PAGE_EXECUTE_READWRITE,
-        &mut PAGE_PROTECTION_FLAGS(0),
+        assembled_new.len(),
+        PAGE_READWRITE,
+        &mut original_flags,
+    )?;
+    target_memory.copy_from_slice(&assembled_new);
+    VirtualProtect(
+        transmute(address),
+        assembled_new.len(),
+        original_flags,
+        &mut original_flags,
     )?;
 
-    target_memory.copy_from_slice(&assembled);
-
-    logln!("Patched  {assembled_len:3} bytes at 0x{address:0X}.");
+    logln!("Patched  {assembled_new_len:3} bytes at 0x{address:0X}.");
 
     Ok(())
 }
@@ -119,15 +148,17 @@ unsafe fn patch_asm(
 pub fn install() -> Result<(), eyre::Error> {
     logln!("Assembling and installing hooks.");
 
-    use iced_x86::code_asm::{ebp, esi, esp};
+    use iced_x86::code_asm::{eax, ebp, esi, esp};
 
     // This hook is very near <SOMETHING, I'M NOW CONFUSED WHAT>.
     // The hook must call `GAME_STATE_TRANSITION_TARGET()` to restore the
     // function call we're overwriting with the hook.
     logln!("Patching game state transition.");
     unsafe {
-        patch_asm(0x4_2A343, |asm| {
-            asm.call(hook as u64)?;
+        patch_asm(0x4_2A343, |old, new| {
+            old.call(*GAME_STATE_TRANSITION_TARGET as u64)?;
+
+            new.call(hook as u64)?;
 
             extern fn hook() {
                 try_or_die(|| {
@@ -167,26 +198,20 @@ pub fn install() -> Result<(), eyre::Error> {
     // so that it works every time instead of toggling.
     logln!("Patching \"day\" cheat code.");
     unsafe {
-        patch_asm(0x4_160AD, |asm| {
-            // iced_x86 expects a u64 for absolute addresses, even though
-            // this program and the assembler are both targeting 32-bit.
-            asm.call(day_cheat_hook as u64)?;
+        patch_asm(0x4_160AD, |old, new| {
+            old.call(0x4_20480)?;
+            old.cmp(ax, 3)?;
+            old.jnz(0x4_1622A)?;
+            old.pop(esi)?;
+            old.pop(ebp)?;
+            old.add(esp, 0x80)?;
+            old.ret()?;
 
-            // After calling our function, we immediately return from the patched
-            // function to avoid running any subsequent instructions (which may
-            // no longer even decode as real instructions, since we may have just
-            // overwritten the first bytes of one and screwed up their alignment).
-            // We copied these instructions from other returns in the function.
-            //
-            // Restore two registers, which have been used for local variables in
-            // this function, to their values from before the function was called.
-            asm.pop(esi)?;
-            asm.pop(ebp)?;
-            // Adjust the stack to remove 128 bytes that had been allocated for
-            // a too-large-for-registers local variable within the function.
-            asm.add(esp, 0x80)?;
-            // Return
-            asm.ret()?;
+            new.call(day_cheat_hook as u64)?;
+            new.pop(esi)?;
+            new.pop(ebp)?;
+            new.add(esp, 0x80)?;
+            new.ret()?;
 
             Ok(())
         })?;
