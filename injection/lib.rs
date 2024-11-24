c10167809e6b188d0f9249cd7801f3d094d74b5c
Jeremy Banks <_@jeremy.ca>
2024-11-24 00:07:26 -0500

    shitty timer

--- injection/lib.rs
+++ injection/lib.rs
@@ -4,14 +4,13 @@ use {
     eyre,
     iced_x86::{
         self,
-        code_asm::{ax, CodeAssembler},
+        code_asm::{ax, ebp, esi, esp, CodeAssembler},
     },
     std::{
-        cell::Cell,
         ffi::CString,
         mem::transmute,
         sync::{
-            atomic::{self, AtomicU64},
+            atomic::{self, AtomicU64, Ordering},
             Mutex,
         },
         time::Instant,
@@ -43,6 +42,23 @@ macro_rules! wcprintln {
         }
     };
 }
+
+macro_rules! wcstatus {
+    ($($arg:tt)*) => {
+        {
+            let message_string = format!($($arg)*);
+
+            let state = unsafe { GAME_STATE.get().read_volatile() };
+
+            if GameState::InGame == state {
+                let message_cstring = CString::new(message_string).unwrap_or(c"<unable to encode as CString>".into());
+                let message_pointer = message_cstring.as_ptr();
+                DISPLAY_MESSAGE(message_pointer, MAX_HUMAN_PLAYERS, 0);
+            }
+        }
+    };
+}
+
 /// This hook replaces the default behaviour of the `day` cheat code.
 extern fn day_cheat_hook() {
     try_or_die(|| {
@@ -72,19 +88,6 @@ extern fn day_cheat_hook() {
     })
 }
 
-extern fn main_loop_hook() {
-    static MAIN_LOOP_TICKS: AtomicU64 = AtomicU64::new(0);
-
-    try_or_die(|| {
-        Ok({
-            let ticks = MAIN_LOOP_TICKS.fetch_add(1, atomic::Ordering::Acquire);
-            if ticks % 1 == 0 {
-                logln!("Main loop hook tick {ticks}.");
-            }
-        })
-    });
-}
-
 unsafe fn patch_asm(
     address: u32,
     asm: impl Fn(&mut CodeAssembler, &mut CodeAssembler) -> Result<(), eyre::Error>,
@@ -97,27 +100,27 @@ unsafe fn patch_asm(
     asm(&mut old_assembler, &mut new_assembler)?;
 
     let assembled_old = old_assembler.assemble(u64::from(address))?;
-    let assembled_new = new_assembler.assemble(u64::from(address))?;
+    let mut assembled_new = new_assembler.assemble(u64::from(address))?;
 
-    let assembled_old_len = assembled_old.len();
+    let assembled_len = assembled_old.len();
     let assembled_new_len = assembled_new.len();
 
-    // TODO: we should allow the old to be longer than the new, and that's how
-    // we deal with mis-alignment. As long as we know everything we're
-    // replacing, it's acceptable.
-    if assembled_old_len < assembled_new_len {
+    if assembled_len < assembled_new_len {
         logln!(
-            "WARN: Assembled original instructions are shorter ({assembled_old_len}B) as \
-             assembled new instructions ({assembled_new_len}B)."
+            "WARN: Assembled original instructions are shorter ({assembled_len}B) as assembled \
+             new instructions ({assembled_new_len}B)."
         )
     }
 
-    logln!("Patching {assembled_new_len:3} bytes at 0x{address:0X}.");
+    // Pad it with NOPs.
+    assembled_new.resize(assembled_len, 0);
+
+    logln!("Patching {assembled_len:3} bytes at 0x{address:0X}.");
 
     let target_memory: &mut [u8] =
-        std::slice::from_raw_parts_mut(transmute(address), assembled_new_len);
+        std::slice::from_raw_parts_mut(transmute(address), assembled_len);
 
-    if target_memory != &assembled_old[..assembled_new_len] {
+    if target_memory != &assembled_old {
         logln!(
             "WARN: Assembled original instructions did not match actual instructions in memory."
         );
@@ -129,71 +132,25 @@ unsafe fn patch_asm(
     let mut original_flags = PAGE_PROTECTION_FLAGS(0);
     VirtualProtect(
         transmute(address),
-        assembled_new.len(),
+        assembled_len,
         PAGE_READWRITE,
         &mut original_flags,
     )?;
     target_memory.copy_from_slice(&assembled_new);
     VirtualProtect(
         transmute(address),
-        assembled_new.len(),
+        assembled_len,
         original_flags,
         &mut original_flags,
     )?;
 
-    logln!("Patched  {assembled_new_len:3} bytes at 0x{address:0X}.");
+    logln!("Patched  {assembled_len:3} bytes at 0x{address:0X}.");
 
     Ok(())
 }
 
 pub fn install() -> Result<(), eyre::Error> {
     logln!("Assembling and installing hooks.");
-
-    use iced_x86::code_asm::{eax, ebp, esi, esp};
-
-    // This hook is very near <SOMETHING, I'M NOW CONFUSED WHAT>.
-    // The hook must call `GAME_STATE_TRANSITION_TARGET()` to restore the
-    // function call we're overwriting with the hook.
-    logln!("Patching game state transition.");
-    unsafe {
-        patch_asm(0x4_2A343, |old, new| {
-            old.call(*GAME_STATE_TRANSITION_TARGET as u64)?;
-
-            new.call(hook as u64)?;
-
-            extern fn hook() {
-                try_or_die(|| {
-                    Ok({
-                        static LAST_TRANSITION: Mutex<Option<Instant>> = Mutex::new(None);
-
-                        let mut last_transition = LAST_TRANSITION.lock().unwrap();
-                        let now = Instant::now();
-                        let state = unsafe { GAME_STATE.get().read_volatile() };
-                        let state = format!("{state:?}");
-
-                        if let Some(last_transition) = *last_transition {
-                            let elapsed = now - last_transition;
-
-                            let seconds = elapsed.as_secs();
-                            let minutes = seconds / 60;
-                            let seconds = seconds % 60;
-                            let millis = elapsed.subsec_millis();
-
-                            logln!("[{state:12}] after {minutes:2}m {seconds:2}.{millis:03}s");
-                        } else {
-                            logln!("[{state:12}]");
-                        }
-
-                        *last_transition = Some(now);
-                    })
-                });
-
-                GAME_STATE_TRANSITION_TARGET();
-            }
-
-            Ok(())
-        })?;
-    }
 
     // Address near the beginning of the 'day' cheat code branch inside the
     // function that applies cheat codes, but after it resets the cheat flags,
@@ -214,6 +171,92 @@ pub fn install() -> Result<(), eyre::Error> {
             new.pop(ebp)?;
             new.add(esp, 0x80)?;
             new.ret()?;
+
+            Ok(())
+        })?;
+    }
+
+    static LAST_TRANSITION: Mutex<Option<Instant>> = Mutex::new(None);
+    static MAIN_LOOP_TICKS: AtomicU64 = AtomicU64::new(0);
+
+    // This hook is very near <SOMETHING, I'M NOW CONFUSED WHAT>.
+    // The hook must call `GAME_STATE_TRANSITION_TARGET()` to restore the
+    // function call we're overwriting with the hook.
+    logln!("Patching game state transition.");
+    unsafe {
+        patch_asm(0x4_2A343, |old, new| {
+            old.call(*GAME_STATE_TRANSITION_TARGET as u64)?;
+
+            new.call(hook as u64)?;
+
+            extern fn hook() {
+                try_or_die(|| {
+                    Ok({
+                        let mut last_transition = LAST_TRANSITION.lock().unwrap();
+                        let now = Instant::now();
+                        let state = unsafe { GAME_STATE.get().read_volatile() };
+                        let state = format!("{state:?}");
+
+                        if let Some(last_transition) = *last_transition {
+                            let elapsed = now - last_transition;
+
+                            let seconds = elapsed.as_secs();
+                            let minutes = seconds / 60;
+                            let seconds = seconds % 60;
+                            let millis = elapsed.subsec_millis();
+
+                            logln!("[{state:12}] after {minutes:2}m {seconds:2}.{millis:03}s");
+                        } else {
+                            logln!("[{state:12}]");
+                        }
+
+                        *last_transition = Some(now);
+
+                        MAIN_LOOP_TICKS.store(0, Ordering::SeqCst);
+                    })
+                });
+
+                GAME_STATE_TRANSITION_TARGET();
+            }
+
+            Ok(())
+        })?;
+    }
+
+    logln!("Patching end of game tick.");
+    unsafe {
+        patch_asm(0x4_212EE, |old, new| {
+            // load game state into AX register through built-in function
+            old.call(0x4_20480)?;
+
+            // load game state into AX register through our hook function
+            new.call(hook as u64)?;
+
+            extern fn hook() -> GameState {
+                let state = unsafe { GAME_STATE.get().read_volatile() };
+
+                try_or_die(|| {
+                    Ok({
+                        let ticks = MAIN_LOOP_TICKS.fetch_add(1, atomic::Ordering::SeqCst);
+
+                        let last_transition = LAST_TRANSITION.lock().unwrap();
+                        let now = Instant::now();
+
+                        if let Some(last_transition) = *last_transition {
+                            let elapsed = now - last_transition;
+
+                            let seconds = elapsed.as_secs();
+                            let minutes = seconds / 60;
+                            let seconds = seconds % 60;
+                            let millis = elapsed.subsec_millis();
+
+                            wcstatus!("{minutes:2}m {seconds:2}.{millis:03}s ({ticks} ticks)");
+                        }
+                    })
+                });
+
+                state
+            }
 
             Ok(())
         })?;
